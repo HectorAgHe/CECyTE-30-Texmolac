c7c09948a95a37ef178b37d56e0c0f2df279da0e
HectorAgHe <aguilarhector440@gmail.com>
2023-11-20 13:27:54 -0600

    correcciones info

--- js/app.js
+++ js/app.js
@@ -6,18 +6,6 @@ window.addEventListener('scroll', function(){
     
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
